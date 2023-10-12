fa8fa3ccf8e2168c404cf71cc4fcb749fa4098a7
connorcrowe-lhl <connor.crowe@lighthouselabs.com>
2023-10-12 19:19:04 -0400

    T3 tail, function outline, test cases, attempt 2

--- tail.js
+++ tail.js
@@ -7,7 +7,10 @@ const tail = function(array) {
 };
 
 const result = tail(['Hello', 'Lighthouse', 'Labs']);
-assertEqual(result.length, 2)
+assertEqual(result.length, 2);
+
+const empty = tail([1]);
+assertEqual(empty, 0);
 
 const words = ['Yo yo', 'Lighthouse', 'Labs'];
 tail(words);
