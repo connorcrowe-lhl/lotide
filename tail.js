d54a80361e4f0244a5be524bc9c02d7c830fbfde
connorcrowe-lhl <connor.crowe@lighthouselabs.com>
2023-10-12 19:22:47 -0400

    T1 for real, tail, function outline

--- tail.js
+++ tail.js
@@ -5,13 +5,3 @@ const assertEqual = function(actual, expected) {
 const tail = function(array) {
 
 };
-
-const result = tail(['Hello', 'Lighthouse', 'Labs']);
-assertEqual(result.length, 2);
-
-const empty = tail([1]);
-assertEqual(empty, 0);
-
-const words = ['Yo yo', 'Lighthouse', 'Labs'];
-tail(words);
-assertEqual(words.length, 3);
