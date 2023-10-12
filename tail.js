785cfa4716df5d577a1bd24d9066736507e77d36
connorcrowe-lhl <connor.crowe@lighthouselabs.com>
2023-10-12 19:13:59 -0400

    T3 tail, function outline, test cases

--- tail.js
+++ tail.js
@@ -5,3 +5,10 @@ const assertEqual = function(actual, expected) {
 const tail = function(array) {
 
 };
+
+const result = tail(['Hello', 'Lighthouse', 'Labs']);
+assertEqual(result.length, 2)
+
+const words = ['Yo yo', 'Lighthouse', 'Labs'];
+tail(words);
+assertEqual(words.length, 3);
