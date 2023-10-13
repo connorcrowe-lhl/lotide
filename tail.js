d429548aa9dfb463c2dfcf17107fa70f161ef8e9
connorcrowe-lhl <connor.crowe@lighthouselabs.com>
2023-10-13 15:48:43 -0400

    T3 function w error, test with error

--- tail.js
+++ tail.js
@@ -1,7 +1,24 @@
 const assertEqual = function(actual, expected) {
-
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
 
 const tail = function(array) {
+  return array[array.length];
+};
 
-};"
+const test1 = tail(['a', 'b']);
+assertEqual(test1, 'b')
+
+const result = tail(['Hello', 'Lighthouse', 'Labs']);
+assertEqual(result.length, 2);
+
+const empty = tail([1]);
+assertEqual(empty, 0);
+
+const words = ['Yo yo', 'Lighthouse', 'Labs'];
+tail(words);
+assertEqual(words.length, 3);
