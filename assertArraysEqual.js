1164ed8c14efb4da604d815e4808b0f1064c384d
connorcrowe-lhl <connor.crowe@lighthouselabs.com>
2023-10-12 22:48:01 -0400

    A3 function wrong, test wrong, test missing

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,24 @@
 const eqArrays = function(array1, array2) {
-
+  if (array1.length !== array2.length) return true;
+  for (let i = 0; i < array1.length; i+=1) {
+    if (array1[i] !== array2[i]) {
+      return false
+    }
+  }
+  return true;
 };
 
 const assertArraysEqual = function(array1, array2) {
-
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed`);
+  }
 };
+
+// Two empty arrays
+assertArraysEqual([], [])
+// One empty array
+assertArraysEqual([1,2,3], [])
+// Same integers
+assertArraysEqual([1,2,3], [1,2,3])
