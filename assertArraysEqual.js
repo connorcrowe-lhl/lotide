498ee4e21071b672fce639e1e2f47d7f21c5c793
connorcrowe-lhl <connor.crowe@lighthouselabs.com>
2023-10-12 23:15:55 -0400

    A4 correct function, covering tests

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,7 +2,7 @@ const eqArrays = require('./eqArrays');
 
 // FUNCTION IMPLEMENTATION
 const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array1)) {
+  if (eqArrays(array1, array2)) {
     console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`); // print out if they match
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`); // print out if they don't match
--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,7 +1,19 @@
-const assertArraysEqua = require('../assertArraysEqual');
+const assertArraysEqual = require('../assertArraysEqual');
+
+// Two empty
+assertArraysEqual([], []);
+
+// One empty
+assertArraysEqual([1, 2, 3], []);
 
 // Same arrays
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 
 // Diff arrays
 assertArraysEqual([1, 2, 3], [3, 2, 1]);
+
+// Same arrays
+assertArraysEqual(['a','b','c'], ['a','b','c']);
+
+// Diff arrays
+assertArraysEqual(['a','b','c'], ['a','b']);
