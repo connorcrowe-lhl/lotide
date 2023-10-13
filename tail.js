c7baedf9230e457a7d01237b09198956737fcd84
connorcrowe-lhl <connor.crowe@lighthouselabs.com>
2023-10-12 22:30:44 -0400

    T4 if these test cases aren't sufficient i swear

--- tail.js
+++ tail.js
@@ -10,24 +10,26 @@ const tail = function(array) {
   return array.slice(1);
 };
 
-const test1 = tail(['a', 'b']);
-assertEqual(test1[0], 'b')
+// Empty array
+const empty = tail([]);
+assertEqual(empty.length, 0);
 
-const test2 = tail([]);
-assertEqual(test2.length, 0);
+// Arr len 1
+const len1 = tail([1]);
+assertEqual(len1.length, 0);
 
-const test3 = tail([1]);
-assertEqual(test3.length, 0);
+// Arr len 2
+const len2 = tail(['a', 'b']);
+assertEqual(len2[0], 'b');
+assertEqual(len2.length, 1);
 
-const test4 = tail(['Hello', 'Lighthouse', 'Labs']);
-assertEqual(test4.length, 2);
+// Arr len 3
+const len3 = tail(['Hello', 'Lighthouse', 'Labs']);
+assertEqual(len3[0], 'Lighthouse');
+assertEqual(len3.length, 2);
 
+// Orignial array unmodified
 const words = ['Yo yo', 'Lighthouse', 'Labs'];
 tail(words);
 assertEqual(words.length, 3);
-
-const words2 = ['New', 'Lighthouse', 'Labs'];
-assertEqual(tail(words2)[0], 'Lighthouse');
-
-const words3 = ['Woo', 'Lighthouse', 'Labs'];
-assertEqual(tail(words3).length, words3.length -1);
+assertEqual(words[0], 'Yo yo');
