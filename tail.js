153ee53b4755e203750f79e2395d3db700e62462
connorcrowe-lhl <connor.crowe@lighthouselabs.com>
2023-10-13 15:44:22 -0400

    T2 test function outline (LLM updated)

--- tail.js
+++ tail.js
@@ -1,31 +1,7 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
+
 };
 
 const tail = function(array) {
-  return array.slice(1);
-};
 
-// Empty array
-assertEqual(tail([]).length, 0);
-
-// Arr len 1
-assertEqual(tail([1]).length, 0);
-
-// Arr len 2
-assertEqual(tail(['a', 'b'])[0], 'b');
-assertEqual(tail(['a', 'b']).length, 1);
-
-// Arr len 3
-assertEqual(tail(['Hello', 'Lighthouse', 'Labs'])[0], 'Lighthouse');
-assertEqual(tail(['Hello', 'Lighthouse', 'Labs']).length, 2);
-
-// Orignial array unmodified
-const words = ['Yo yo', 'Lighthouse', 'Labs'];
-tail(words);
-assertEqual(words.length, 3);
-assertEqual(words[0], 'Yo yo');
+};"
