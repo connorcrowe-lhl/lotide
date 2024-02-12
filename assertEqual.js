6dd1759fb29ce7b5905c64241768f1b45b442f1d
unknown <connor.crowe@lighthouselabs.com>
2024-02-12 10:10:03 -0500

    Getting example of AIF for screenshot

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,6 @@
 const assertEqual = function(actual, expected) {
 
-  if (actual === expected) {
+  if (actual == expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`); // print out if they match
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`); // print out if they don't match
